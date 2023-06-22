b9d21437f9437332b13e0e9aba28de958dcab194
Lavodix <mouhcineattif@gmail.com>
2023-06-22 22:30:14 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int is_uppercase(int c);
+
+int main() {
+	    int result1 = is_uppercase('W');
+	        int result2 = is_uppercase('a');
+		    
+		    printf("%d\n", result1);  // Output number 1 which we have it already the "W"
+					      //     printf("%d\n", result2);  // Output number 2 which we have it already the "a"
+					      //         
+					      //             return 0;
+					      //             }
+					      //
+					      //             int is_uppercase(int c) {
+					      //                 if (c >= 'A' && c <= 'Z') //this is the rang of the alphabethics  
+					      //                     {
+					      //                             return 10;
+					      //                                 } else {
+					      //                                         return 1;
+					      //                                             }
+					      //                                             }
+					      //                                             //end of the programme
